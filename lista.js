257223fa2a1de249704531cd21fd0ebc4f2edf2a
AugustoFukumoto <120630826+AugustoFukumoto@users.noreply.github.com>
2023-02-06 22:04:36 +0900

    Update lista.js

--- lista.js
+++ lista.js
@@ -13,3 +13,12 @@ nomes.splice(1, 2);
 console.log(nomes);
 
 
+const alunos = ['joao', 'juliana', 'ana', 'caio'];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunoseMedia = [alunos, medias];
+
+//console.log(listaDeAlunoseMedia);
+
+console.log(`A aluna da posição 2 da lista de alunos é: ${listaDeAlunoseMedia[0][2]}. 
+anota dessa aluna é: ${listaDeAlunoseMedia[1][2]} `);
